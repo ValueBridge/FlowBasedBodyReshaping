163de6cbdd05f426b311a6861b8f49345f80cdb5
Kuba Kolodziejczyk <kuba@conceptual.jp>
2023-12-14 22:07:50 +0900

    Stubbed out docker environment
    Added files to build docker image on both linux and osx, added basic invoke commands

---
+++ docker/app.Dockerfile
@@ -0,0 +1,18 @@
+FROM python:3.10.13-bookworm
+
+RUN apt update && apt install -y libcairo2-dev libgl1 libgl1-mesa-dev libosmesa6-dev
+
+ARG REQUIREMENTS_FILE
+
+# Install python environment
+COPY ./${REQUIREMENTS_FILE} /tmp/requirements.txt
+RUN pip install -r /tmp/requirements.txt
+
+# Setup bashrc
+COPY ./docker/bashrc /root/.bashrc
+
+# Setup PYTHONPATH
+ENV PYTHONPATH=.
+
+# # Set up working directory
+WORKDIR /app
---
+++ photobridge/invoke/visualize.py
@@ -0,0 +1,56 @@
+"""
+Module with visualization commands
+"""
+
+import invoke
+
+
+@invoke.task
+def visualize_body_reshaping(_context, config_path):
+    """
+    Visualize mesh predictions
+
+    Args:
+        _context (invoke.Context): context instance
+        config_path (str): path to configuration file
+    """
+
+    import glob
+    import os
+
+    import toml
+    import tqdm
+
+    import config.test_config
+    import reshape_base_algos.body_retoucher
+
+
+    import photobridge.utilities
+
+    configuration = photobridge.utilities.read_yaml(config_path)
+
+    reshaper_config_path = "config/test_cvpr_setting.toml"
+    reshaper_default_config = config.test_config.TESTCONFIG
+
+    with open(reshaper_config_path) as file:
+
+        reshaper_config = toml.load(file)
+
+        reshaper_config = config.test_config.load_config(
+            custom_config=reshaper_config,
+            default_config=reshaper_default_config)
+
+    retoucher = reshape_base_algos.body_retoucher.BodyRetoucher.init(
+        reshape_ckpt_path=reshaper_default_config.reshape_ckpt_path,
+        pose_estimation_ckpt=reshaper_default_config.pose_estimation_ckpt,
+        device=0, log_level='error',
+        log_path='test_log.txt',
+        debug_level=0)
+
+    print(retoucher)
+
+    image_paths = glob.glob(os.path.join(configuration.input_images_directory, "*.jpg"))
+
+    for image_path in tqdm.tqdm(image_paths):
+
+        print(image_path)
